064710f4ee66be5c8690acbbe81f4dd870580bfd
ksitu0 <karenhj.situ@gmail.com>
2022-01-30 15:07:28 -0800

    jitsi room woks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import './assets/textStyles.css';
+import Modal from 'react-modal';
+import { Field, Form, Formik } from 'formik';
+import { useState } from 'react';
+
+import Chat from "./components/chat"
+
+Modal.setAppElement('body');
 
 function App() {
-  return (
+  const [user, setUser] = useState("");
+  const [isOpen, setIsOpen] = useState(true);
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {/* <Modal
+        isOpen={isOpen}
+        style={{width: "100%"}}
+      >
+        <Formik
+          initialValues={{ user: ""}}
+          onSubmit={(values, actions) => {
+            setUser(values.user);
+            setIsOpen(false);
+            actions.setSubmitting(false);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Form>
+            <Field
+              id="user"
+              name="user"
+              type="text"
+              placeholder="Your name"
+            />
+      
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
+      </Modal> */}
+      <div style={{backgroundColor: "lightblue"}}>
+        Upload your thing
+      </div>
+      <Chat user={user} />
     </div>
   );
 }
---
+++ src/components/chat.js
@@ -0,0 +1,40 @@
+import '../assets/textStyles.css';
+// import { Widget } from 'react-chat-widget';
+import { useState } from 'react';
+import { collection, addDoc, onSnapshot, getDocs, query, Timestamp, orderBy, limit} from "firebase/firestore";
+import { db } from "../config/firebase";
+
+import Jitsi from 'react-jitsi';
+
+function Chat(props) {
+  const [displayName, setDisplayName] = useState('')
+  const [roomName, setRoomName] = useState('')
+  const [password, setPassword] = useState('')
+  const [onCall, setOnCall] = useState(false)
+ 
+  return onCall
+    ? (
+      <Jitsi
+        roomName={roomName}
+        displayName={displayName}
+        // password={password}
+        // loadingComponent={}
+        config={{prejoinPageEnabled: false}}
+        containerStyle={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+      }}
+        onAPILoad={JitsiMeetAPI => console.log('Good Morning everyone!')}
+      />)
+    : (
+      <>
+        <h1>Crate a Meeting</h1>
+        <input type='text' placeholder='Room name' value={roomName} onChange={e => setRoomName(e.target.value)} />
+        <input type='text' placeholder='Your name' value={displayName} onChange={e => setDisplayName(e.target.value)} />
+        <button onClick={() => setOnCall(true)}> Let&apos;s start!</button>
+      </>
+    )
+}
+
+export default Chat;
